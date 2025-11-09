69b61efd5047cacb637d30d1e16924a25429ab3e
C Magnus Berglund <cmagnusb@gmail.com>
2025-11-09 05:38:43 +0100

    "Test skeleton and comment line on hibernate DDL=update"

---
+++ shop-service/src/test/java/com/perfect8/shop/entity/AddressTest.java
@@ -0,0 +1,11 @@
+package com.perfect8.shop.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class AddressTest {
+
+    @Test
+    void builder() {
+    }
+}
