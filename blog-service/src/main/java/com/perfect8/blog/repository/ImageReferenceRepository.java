10afa3511b38428c98db561f01e491d507ff9f83
C Magnus Berglund <cmagnusb@gmail.com>
2025-11-12 13:25:20 +0100

    findByPostId changed to findByPost_PostId and removal of hibernate.dialect property

--- blog-service/src/main/java/com/perfect8/blog/repository/ImageReferenceRepository.java
+++ blog-service/src/main/java/com/perfect8/blog/repository/ImageReferenceRepository.java
@@ -1,14 +1,29 @@
-// blog-service/src/main/java/com/perfect8/blog/repository/ImageReferenceRepository.java
-
-        package com.perfect8.blog.repository;
+package com.perfect8.blog.repository;
 
 import com.perfect8.blog.model.ImageReference;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
+/**
+ * Repository for ImageReference entity
+ * 
+ * FIXED (2025-11-12):
+ * - findByPostId â†' findByPost_PostId (explicit path to Post.postId)
+ * - deleteByImageId â†' takes Long (imageId is Long, not String)
+ */
 @Repository
 public interface ImageReferenceRepository extends JpaRepository<ImageReference, Long> {
-    List<ImageReference> findByPostId(Long postId);
-    void deleteByImageId(String imageId);
+    
+    /**
+     * Find all image references for a specific post
+     * Uses explicit property path: ImageReference.post.postId
+     */
+    List<ImageReference> findByPost_PostId(Long postId);
+    
+    /**
+     * Delete image references by imageId
+     * imageId is Long (FK to image-service Image.imageId)
+     */
+    void deleteByImageId(Long imageId);
 }
