06e4b7e5f164616d277711f7ba7d93da3d7fa7e7
C Magnus Berglund <cmagnusb@gmail.com>
2025-12-06 14:31:19 +0100

    Fix: Exclude UserDetailsServiceAutoConfiguration from image-service

--- image-service/src/main/java/com/perfect8/image/ImageServiceApplication.java
+++ image-service/src/main/java/com/perfect8/image/ImageServiceApplication.java
@@ -3,7 +3,9 @@ package com.perfect8.image;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration.class
+})
 public class ImageServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ImageServiceApplication.class, args);
