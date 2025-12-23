a6b99f9ce1d5bca4e38fe4d9b03a595a16a7f686
C Magnus Berglund <cmagnusb@gmail.com>
2025-12-23 14:43:40 +0100

    check email endpoint

--- email-service/src/main/java/com/perfect8/email/controller/EmailController.java
+++ email-service/src/main/java/com/perfect8/email/controller/EmailController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
  */
 @Slf4j
 @RestController
-@RequestMapping("/api/email")
+@RequestMapping("/api/v1/email")
 @RequiredArgsConstructor
 public class EmailController {
 
